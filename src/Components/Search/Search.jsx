f3ef7a4c3e60b44897a776915cbe5f829cc02c5c
Jeongeum <wjddma1202@naver.com>
2023-04-21 11:21:37 +0900

    design : Search 컴포넌트 마크업 및 css 적용
    타이틀, 검색, 레벨 버튼 마크업 및 css

---
+++ src/Components/Search/Search.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import styled from "styled-components";
+import { LevelButton } from "../LevelButton/LevelButton";
+
+const SearchWrapper = styled.section`
+  text-align: center;
+  margin-bottom: 2rem;
+`;
+
+const SearchTitle = styled.h2`
+  font-size: 2rem;
+  margin-bottom: 2rem;
+`;
+
+const SearchInput = styled.input`
+  width: 65%;
+  height: 2.5rem;
+  border: none;
+  border-bottom: 1px solid #d7e2eb;
+  margin-right: 1rem;
+  font-size: 1.5rem;
+
+  &:focus {
+    outline: none;
+    border-bottom: 1px solid #0078ff;
+  }
+`;
+
+const SearchBtn = styled.button`
+  width: 55px;
+  padding: 0.5rem;
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
+  margin-top: 1.3rem;
+`;
+const BtnWrapper = styled.div`
+  margin-top: 1.3rem;
+`;
+
+export const Search = () => {
+  return (
+    <SearchWrapper>
+      <SearchTitle>프로그래머스 문제 검색 사이트</SearchTitle>
+      <form>
+        <SearchInput type="text"></SearchInput>
+        <SearchBtn>검색</SearchBtn>
+      </form>
+      <BtnWrapper>
+        <LevelButton />
+        <LevelButton />
+        <LevelButton />
+        <LevelButton />
+        <LevelButton />
+      </BtnWrapper>
+    </SearchWrapper>
+  );
+};
