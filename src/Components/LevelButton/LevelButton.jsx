1ab947a6e8ae803f46defb6a94bcdbcc3a443370
Jeongeum <wjddma1202@naver.com>
2023-04-21 11:22:04 +0900

    design : 레벨버튼 컴포넌트 마크업 및 css 적용

---
+++ src/Components/LevelButton/LevelButton.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+
+const LevelBtn = styled.button`
+  width: 55px;
+  padding: 0.5rem;
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
+
+  background-color: #475c73;
+  color: #fff;
+
+  &:focus {
+    background-color: #0078ff;
+  }
+`;
+export const LevelButton = () => {
+  return <LevelBtn>Lv.0</LevelBtn>;
+};
